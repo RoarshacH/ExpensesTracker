eceb9d29163cf69b944befb34f933f21abe69003
RoarshacH <smv.dilshan@gmail.com>
2022-03-15 20:22:21 -0400

    Analytics Finish

--- app/src/main/java/com/example/expesestracker/AnalyticActivity.kt
+++ app/src/main/java/com/example/expesestracker/AnalyticActivity.kt
@@ -3,9 +3,15 @@ package com.example.expesestracker
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.expesestracker.models.Constants
 import com.example.expesestracker.models.DBUtilities
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -19,47 +25,94 @@ import com.github.mikephil.charting.utils.ColorTemplate
 class AnalyticActivity : AppCompatActivity() {
 
     var util = DBUtilities()
+    var hashMap: HashMap<String, Float> = HashMap()
+    var typeSelection = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analytic)
 
         val pieChart = findViewById<PieChart>(R.id.pieChart)
+        val spinner = findViewById<Spinner>(R.id.selectTimeSpinner)
 
-        var hashMapExpensesWeek : HashMap<String, Float> = util.getThisWeekExpenses(this.getSharedPreferences("test-expenses", Context.MODE_PRIVATE))
-        if (hashMapExpensesWeek.isNotEmpty()) {
-            val entries: ArrayList<PieEntry> = ArrayList()
+        val legend = pieChart.legend
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        legend.orientation = Legend.LegendOrientation.VERTICAL
+        legend.textSize = 16F
 
-            for (key in hashMapExpensesWeek.keys) {
-                if (key.equals("Total")){
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setUsePercentValues(true)
+        pieChart.setEntryLabelTextSize(0F)
+        pieChart.setCenterTextSize(16F)
+
+        val colors: ArrayList<Int> = ArrayList()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                hashMap = getDBData(position)
+                when(position){
+                    0 -> {
+                        pieChart.centerText = "This Weeks Spending"
+                    }
+                    1 -> {
+                        pieChart.centerText = "This Months Spending"
+                    }
+                    2 -> {
+                        pieChart.centerText = "This Weeks Incomes"
+                    }
+                    3 -> {
+                        pieChart.centerText = "This Months Incomes"
+                    }
                 }
-                else{
-                    entries.add(PieEntry(hashMapExpensesWeek[key]!!, key))
+                if (hashMap.isNotEmpty()){
+                    val entries = getPieData(hashMap)
+                    val dataset = PieDataSet(entries, typeSelection)
+                    dataset.colors = colors
+
+                    val data = PieData(dataset)
+                    data.setDrawValues(true)
+                    data.setValueFormatter(PercentFormatter(pieChart))
+                    data.setValueTextSize(12f)
+                    data.setValueTextColor(Color.BLUE)
+
+                    pieChart.data = data
+                    pieChart.invalidate()
                 }
             }
 
-            val colors: ArrayList<Int> = ArrayList()
-
-            for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
-            for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
-
-            val dataset = PieDataSet(entries, "Expenses")
-            dataset.colors = colors
-
-            val data = PieData(dataset)
-            data.setDrawValues(true)
-            data.setValueFormatter(PercentFormatter(pieChart))
-            data.setValueTextSize(12f)
-            data.setValueTextColor(Color.BLUE)
-
-            pieChart.data = data
-            pieChart.invalidate()
-
-            pieChart.isDrawHoleEnabled = true
-            pieChart.setUsePercentValues(true)
-
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
         }
 
+    }
 
+    private fun getPieData(values : HashMap<String, Float>): ArrayList<PieEntry> {
+        val entries: ArrayList<PieEntry> = ArrayList()
+        for (key in values.keys) {
+            if (key.equals("Total")){
+            }
+            else{
+                entries.add(PieEntry(values[key]!!, key))
+            }
+        }
+        return entries
+    }
+
+    private fun getDBData(position: Int): HashMap<String, Float> {
+        var hashMap : HashMap<String, Float> = HashMap()
+        when(position){
+            0 -> hashMap = util.getThisWeekExpenses(this.getSharedPreferences("test-expenses", Context.MODE_PRIVATE))
+            1 -> hashMap = util.getThisMonthExpenses(this.getSharedPreferences("test-expenses", Context.MODE_PRIVATE))
+            2 -> hashMap = util.getThisWeekIncomes(this.getSharedPreferences("test_income", Context.MODE_PRIVATE))
+            3 -> hashMap = util.getThisMonthIncomes(this.getSharedPreferences("test_income", Context.MODE_PRIVATE))
+        }
+        return hashMap
 
     }
 }

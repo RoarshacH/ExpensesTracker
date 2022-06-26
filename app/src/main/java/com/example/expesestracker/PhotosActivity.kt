aa26777725a7833f8935925c160ee0a06acbb67a
RoarshacH <smv.dilshan@gmail.com>
2022-06-26 18:27:38 -0400

    Images List is done

--- app/src/main/java/com/example/expesestracker/PhotosActivity.kt
+++ app/src/main/java/com/example/expesestracker/PhotosActivity.kt
@@ -1,36 +1,55 @@
 package com.example.expesestracker
 
+import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.expesestracker.models.Image
+import com.example.expesestracker.models.ImageAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class PhotosActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 100
-    lateinit var pictureImageView : ImageView
+    lateinit var pictureRecyclerView : RecyclerView
+    var progressbar: ProgressBar?= null
     lateinit var imageUri : Uri
+    private var picturesList: ArrayList<Image> ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photos)
-        pictureImageView = findViewById(R.id.pictureImageView)
+        pictureRecyclerView = findViewById(R.id.pictureRecyclerView)
+        progressbar = findViewById(R.id.imagesProgress)
 
-        val btnTakePicture = findViewById<Button>(R.id.btnTakePicture);
+        pictureRecyclerView?.layoutManager= GridLayoutManager(this, 3)
+        pictureRecyclerView?.setHasFixedSize(true)
+
+
+        requestPermissions();
+
+        val btnTakePicture = findViewById<FloatingActionButton>(R.id.fabTakePicture);
         btnTakePicture.setOnClickListener {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             val imagePath = createImage()
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imagePath)
-
 
             try{
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
@@ -40,8 +59,38 @@ class PhotosActivity : AppCompatActivity() {
             }
         }
 
+        picturesList = ArrayList()
 
+        if (picturesList!!.isEmpty()){
+            progressbar?.visibility = View.VISIBLE
+            picturesList = getAllImages()
+            pictureRecyclerView?.adapter = ImageAdapter(this, picturesList!!)
+            progressbar?.visibility = View.GONE
+        }
 
+    }
+
+    private fun getAllImages(): ArrayList<Image>? {
+        val images = ArrayList<Image>()
+        val allImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(MediaStore.Images.ImageColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME)
+        var cursor = this@PhotosActivity.contentResolver.query(allImageUri,projection,null,null, null)
+
+        try {
+            cursor!!.moveToFirst()
+            do {
+                val image = Image()
+                image.imagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                image.imageName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
+                images.add(image)
+            }while (cursor.moveToNext())
+            cursor.close()
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+        return images
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,8 +106,21 @@ class PhotosActivity : AppCompatActivity() {
 
     }
 
+    private fun requestPermissions(){
+        if(ContextCompat.checkSelfPermission(this@PhotosActivity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this@PhotosActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),101)
+        }
 
-    fun createImage(): Uri? {
+        if(ContextCompat.checkSelfPermission(this@PhotosActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this@PhotosActivity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),102)
+        }
+
+    }
+
+
+    private fun createImage(): Uri? {
         var uri: Uri? = null
         var contentResolver = contentResolver
         val imgName: String = System.currentTimeMillis().toString()
---
+++ app/src/main/java/com/example/expesestracker/models/ImageAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.expesestracker.models
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.expesestracker.R
+
+class ImageAdapter(private var context: Context, private var imagesList: ArrayList<Image>):
+    RecyclerView.Adapter<ImageAdapter.ViewHolder>()  {
+
+    var imageVew: ImageView? = null
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val imageVew = itemView.findViewById<ImageView>(R.id.row_image)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_image_item, parent,false)
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentImage = imagesList[position]
+
+        Glide.with(context)
+            .load(currentImage.imagePath)
+            .apply(RequestOptions().centerCrop())
+            .into(holder.imageVew!!)
+
+
+        holder.imageVew.setOnClickListener{
+            Toast.makeText(context, "Image Clicked" , Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    override fun getItemCount(): Int {
+        return imagesList.size
+    }
+
+
+}
